e9d632de8dd78db9c867316eb7be95190a58c410
nguyencongduc <n.c.duc@itgtechnology.vn>
2025-05-04 18:35:26 +0700

    playlist

---
+++ frontend/src/service/PlaylistService.js
@@ -0,0 +1,39 @@
+// src/services/albumService.js
+import axios from "axios";
+import axiosInstance from "../api/axiosInstance";
+
+// src/service/albumService.js
+export const createPlaylistAPI = async (obj) => {
+  try {
+    const response = await axiosInstance.post("/music/playlists/",obj);
+    return response;
+  } catch (error) {
+    console.error("Error fetching all albums:", error);
+    return [];
+  }
+};
+
+// src/service/songService.js
+export const fetchAllPlaylist = async () => {
+  try {
+    const response = await axiosInstance.get("/music/playlists/?user_id="+localStorage.getItem('userId'));
+    // Kiểm tra cả response.data.results và response.data
+    return response;
+  } catch (error) {
+
+    console.error("Error fetching all songs:", error);
+    return [];
+  }
+};
+
+
+export const fetchAllPlaylistById = async (id) => {
+  try {
+    const response = await axiosInstance.get("/music/playlists/"+id+'/');
+    return response;
+  } catch (error) {
+
+    console.error("Error fetching all songs:", error);
+    return [];
+  }
+};
