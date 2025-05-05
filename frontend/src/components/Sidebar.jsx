dcb752ec804535f56f2150e5532acb01ada97980
nguyencongduc <n.c.duc@itgtechnology.vn>
2025-05-05 19:37:01 +0700

    show list music of playlist

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from "react";
+import React, {  useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import {  createPlaylistAPI, fetchAllPlaylist } from "../service/PlaylistService";
 
